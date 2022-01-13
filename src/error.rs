d1eb5bb4103f6fd0f7decbbc3bd076ae01ce4cb6
kwguo <shana0440@gmail.com>
2022-01-13 10:23:39 +0800

    feat: lookup parent directories to find repository

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,7 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum EnvmError {
+    NotEnvmRepository,
     MissingConfigFile,
     FailedToParseConfig,
 }
@@ -9,6 +10,7 @@ pub enum EnvmError {
 impl fmt::Display for EnvmError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match &self {
+            EnvmError::NotEnvmRepository => write!(f, "not a envm repository (or any of the parent directories)"),
             EnvmError::MissingConfigFile => write!(f, "cannot found the configuration at .envm"),
             EnvmError::FailedToParseConfig => write!(f, "failed to parse the configuration"),
         }
--- src/repository.rs
+++ src/repository.rs
@@ -10,7 +10,10 @@ pub struct Repository {
 
 impl Repository {
     pub fn new() -> Result<Repository, EnvmError> {
-        let worktree = env::current_dir().unwrap().join(".envm");
+        let worktree = match lookup_repository(env::current_dir().unwrap()) {
+            Some(path) => path.join(".envm"),
+            None => return Err(EnvmError::NotEnvmRepository),
+        };
         let config_path = worktree.join("config");
         let contents = fs::read_to_string(config_path)
             .map_err(|_| EnvmError::MissingConfigFile)?;
@@ -27,5 +30,27 @@ impl Repository {
 
     pub fn config(&self) -> &Config {
         &self.config
+    }
+}
+
+fn lookup_repository(dir: PathBuf) -> Option<PathBuf> {
+    let mut path = dir.as_path();
+    while !path.join(".envm").exists() {
+        match path.parent() {
+            Some(p) => path = p,
+            None => return None,
+        }
+    }
+    return Some(path.to_path_buf());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn not_a_repository() {
+        let root = Path::new("/not/a/repository").to_path_buf();
+        assert_eq!(lookup_repository(root), None);
     }
 }
